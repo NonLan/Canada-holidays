78c555346e511e2ab5af6d7571bc7bcb2a01d65e
Noelle <95160562+NonLan@users.noreply.github.com>
2024-11-26 13:57:30 -0500

    Update csc309-e11.js

--- pages/csc309-e11.js
+++ pages/csc309-e11.js
@@ -1,7 +1,7 @@
 export default function Holidays() { 
     return (
         <div id="auto-tester">
-        Hi. I am ransomn1, and I have learned how to develop a full-stack web app
+        Hi. I am 1008236749, and I have learned how to develop a full-stack web app
         and deploy it!
         </div>
   )
